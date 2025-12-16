a8f90f9b85728d6794dd5596745d192936253cf8
SomebodyForSomeone <smb_for_sm1@proton.me>
2025-12-16 22:18:12 +0300

    Middleware pattern Designed pattern for future middleware modules development and including

--- internal/WAF/module.go
+++ internal/WAF/module.go
@@ -13,11 +13,37 @@ func Run(port, targetAddress string) {
 		log.Fatalln("Error on parsing target URL:", err)
 	}
 
-	proxy := httputil.NewSingleHostReverseProxy(target)
+
+	middlewares := []Middleware{
+		&SomeCheck{},
+	}
+
+	var handler http.Handler = httputil.NewSingleHostReverseProxy(target)
+	for _, middleware := range middlewares {
+		handler = middleware.push(handler)
+	}
+
 
 	log.Printf("Attemp to create Reverse Proxy on the post %s with redirecting to %s", port, targetAddress)
-	if err := http.ListenAndServe(port, proxy); err != nil {
+	if err := http.ListenAndServe(port, handler); err != nil {
 		log.Fatalln("Error on starting Reverse Proxy:", err)
 	}
 	log.Println("Successful")
+}
+
+
+type Middleware interface {
+	push(h http.Handler) http.Handler
+}
+
+
+type SomeCheck struct {}
+func (m *SomeCheck) push(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("New request: %s", r.RemoteAddr)
+
+		next.ServeHTTP(w, r)
+
+		/// Do smth
+	})
 }
