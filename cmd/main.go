2e818d88e958a193e8a26a49f6b4884a1133d6e7
SomebodyForSomeone <smb_for_sm1@proton.me>
2025-12-16 20:35:03 +0300

    Base Reverse Proxy setup Minimal setup for forwarding requests throw WAF

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,11 @@ import (
 	waf "github.com/SomebodyForSomeone/WAF-lya/internal/WAF"
 )
 
+
+const wafPort string = ":8000"
+const targetAddress string = "http://localhost:8081"
+
+
 func main() {
-	waf.SayHello()
+	waf.Run(wafPort, targetAddress)
 }
--- internal/WAF/module.go
+++ internal/WAF/module.go
@@ -1,7 +1,23 @@
 package waf
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+)
 
-func SayHello() {
-	fmt.Println("Hello")
+func Run(port, targetAddress string) {
+	target, err := url.Parse(targetAddress)
+	if err != nil {
+		log.Fatalln("Error on parsing target URL:", err)
+	}
+
+	proxy := httputil.NewSingleHostReverseProxy(target)
+
+	log.Printf("Attemp to create Reverse Proxy on the post %s with redirecting to %s", port, targetAddress)
+	if err := http.ListenAndServe(port, proxy); err != nil {
+		log.Fatalln("Error on starting Reverse Proxy:", err)
+	}
+	log.Println("Successful")
 }
